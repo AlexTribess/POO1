19b13501dafb4b6edbd45fe97c4a846a23234570
AlexTribess <alexktribess@gmail.com>
2025-08-07 11:13:30 -0300

    Atividade 2 Concluida

---
+++ Atividades/src/Atv2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Atv2 {
+	public static void main(String[] args) {
+		Scanner num = new Scanner(System.in);
+		
+		System.out.print("Informe um número inteiro: ");
+		int numI = num.nextInt();
+		System.out.println("O número informado foi: " + numI);
+		
+		num.close();
+	}
+}
