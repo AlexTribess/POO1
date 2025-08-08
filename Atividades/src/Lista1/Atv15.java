d1b5df6c319013c6d2a98061e1cc7aca5638db07
AlexTribess <alexktribess@gmail.com>
2025-08-08 11:04:23 -0300

    Alterações aleatorias

---
+++ Atividades/src/Lista1/Atv15.java
@@ -0,0 +1,17 @@
+package Lista1;
+
+import java.util.Scanner;
+
+public class Atv15 {
+	public static void main(String[] args) {
+		Scanner num = new Scanner(System.in);
+		
+		System.out.println("Digite a area a ser pintada: ");
+		double area = num.nextDouble();
+		
+		double tinta = 
+		
+		num.close();
+	}
+
+}
