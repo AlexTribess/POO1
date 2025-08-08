9ac6317c768faaff9d593a1e48a3b143e0c9d461
AlexTribess <alexktribess@gmail.com>
2025-08-08 08:29:07 -0300

    Atividade 5 Concluída

---
+++ Atividades/src/Atv5.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Atv5 {
+	public static void main(String[] args) {
+		Scanner metros = new Scanner(System.in);
+		
+		System.out.println("Digite uma medida em metros: ");
+		float m = metros.nextFloat();
+		float cm = m*100;
+		System.out.println("Em centímetros fica: " + cm);
+		
+		metros.close();
+	}
+}
