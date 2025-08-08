a6be6c34a07bb262e6636b8c83e04469d654ddff
AlexTribess <alexktribess@gmail.com>
2025-08-08 11:10:00 -0300

    Atividade 15 concluída

--- Atividades/src/Lista1/Atv15.java
+++ Atividades/src/Lista1/Atv15.java
@@ -5,12 +5,18 @@ import java.util.Scanner;
 public class Atv15 {
 	public static void main(String[] args) {
 		Scanner num = new Scanner(System.in);
-		
+
 		System.out.println("Digite a area a ser pintada: ");
 		double area = num.nextDouble();
-		
-		double tinta = 
-		
+
+		double litros = area / 3;
+
+		double latas = Math.ceil(litros / 18);
+
+		double valor = latas * 80;
+
+		System.out.println("Será preciso comprar: " + latas + " lata(s) com o valor de: R$" + valor);
+
 		num.close();
 	}
 
